ed5f4c51f2efe91af888cb512894cd6768180dae
Keara Soloway <kls286@cornell.edu>
2023-12-05 09:32:20 -0500

    Allow common.NexusWriter to "append" data to existing files
    Add a kwarg for NexusWriter.write: nxpath.
    If nxpath is None (the deafult), behavior of NexusWriter is same as
    before.
    If nxpath is a string, data will be saved underneath the existing
    object at that path in the file.

--- CHAP/common/writer.py
+++ CHAP/common/writer.py
@@ -25,16 +25,47 @@ def write_matplotlibfigure(data, filename, force_overwrite=False):
 
     data.savefig(filename, **savefig_kw)
 
-def write_nexus(data, filename, force_overwrite=False):
+def write_nexus(data, filename, nxpath=None, force_overwrite=False):
+    """Save a NeXus object to a file. One may use the `nxpath` kwarg
+    to save `data` to a particular location in an existing file. If
+    `nxpath` is not used, `data` will be saved as the only member of
+    the root group of `filename`.
+
+    :param data: The NXobject to save
+    :type data: nexusformat.necus.NXobject
+    :param filename: The name of the file to which `data` will be
+        saved
+    :type filename: str
+    :param nxpath: The location in `filename` to which to write
+        `data`, defaults to None
+    :type nxpath: str, optional
+    :param force_overwrite: Indicate whether `filename` should be
+        entirely overwritten if it exists (if `nxpath` is None), or if
+        the NXobect at `nxpath` in `filename` should be overwritten
+        (if `nxpath` is not None), defaults to False
+    :type force_overwrite: bool, optional
+    :raises NexusError: If data cannot be saved to the specified
+        location
+    :returns: None
+    """
     # Third party modules
-    from nexusformat.nexus import NXobject
+    from nexusformat.nexus import NXFile, NXobject
 
     if not isinstance(data, NXobject):
         raise TypeError('Cannot write object of type'
-                        f'{type(data).__name__} as a NeXus file.')
-
-    mode = 'w' if force_overwrite else 'w-'
-    data.save(filename, mode=mode)
+                        f'{type(data).__name__} in a NeXus file.')
+    if nxpath is None:
+        mode = 'w' if force_overwrite else 'w-'
+        data.save(filename, mode=mode)
+    else:
+        with NXFile(filename, 'rw') as nxfile:
+            root = nxfile.readfile()
+            parent_item = root[nxpath]
+            if parent_item.get(data.nxname) is not None:
+                if force_overwrite:
+                    del parent_item[data.nxname]
+            parent_item[data.nxname] = data
+            nxfile.update(parent_item)
 
 def write_tif(data, filename, force_overwrite=False):
     # Third party modules
@@ -120,7 +151,7 @@ def write_filetree(data, outputdir, force_overwrite=False):
                     nxentry.attrs[kk] = vv
                 for kk, vv in v.items():
                     nxentry[kk] = vv
-                write_nexus(nxentry, filename, force_overwrite)
+                write_nexus(nxentry, filename, force_overwrite=force_overwrite)
             else:
                 raise TypeError(f'Files of type {schema} not yet implemented')
         elif isinstance(v, NXgroup):
@@ -176,18 +207,27 @@ class MatplotlibFigureWriter(Writer):
 
 class NexusWriter(Writer):
     """Writer for NeXus files from `NXobject`-s"""
-    def write(self, data, filename, force_overwrite=False):
+    def write(self, data, filename,
+              nxpath=None, force_overwrite=False):
         """Write `data` to a NeXus file
 
         :param data: the data to write to `filename`.
         :type data: nexusformat.nexus.NXobject
         :param filename: name of the file to write to.
+        :param nxpath: Specific location within `filename` to write
+            `data` to. Use this argument to add an NXobject to an
+            existing NeXus file without overwriting other
+            non-conflicting objects in the file. A conflicting object
+            (an object at the same path as `nxpath` AND the same
+            `nxname` as `data`) can be overwritten if
+            `force_overwrite` is True.
         :param force_overwrite: flag to allow data in `filename` to be
             overwritten, if it already exists.
         :return: the original input data
         """
         data = self.unwrap_pipelinedata(data)[-1]
-        write_nexus(data, filename, force_overwrite)
+        write_nexus(data, filename, nxpath=nxpath,
+                    force_overwrite=force_overwrite)
         return data
 
 
